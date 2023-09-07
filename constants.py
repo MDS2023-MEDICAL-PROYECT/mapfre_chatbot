1c001688f745f6513e150a4831d2ba325ad2fc55
Bernardo Palenciano <b_palenciano_vizcaino@hotmail.com>
2023-09-07 17:51:36 +0200

    streamlit secrets

--- constants.py
+++ constants.py
@@ -1,12 +1,11 @@
 from typing import Final
+import streamlit as st
 
-from os import environ
+# from os import environ
+# from dotenv import load_dotenv
+# load_dotenv()
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-PINECONE_API_KEY: Final[str] = environ["PINECONE_API_KEY"]
-PINECONE_API_ENV: Final[str] = environ["PINECONE_API_ENV"]
-OPENAI_API_KEY: Final[str] = environ["OPENAI_API_KEY"]
+PINECONE_API_KEY: Final[str] = st.secrets["PINECONE_API_KEY"]
+PINECONE_API_ENV: Final[str] = st.secrets["PINECONE_API_ENV"]
+OPENAI_API_KEY: Final[str] = st.secrets["OPENAI_API_KEY"]
 INDEX_NAME: Final[str] = 'thevalleyopenai'
