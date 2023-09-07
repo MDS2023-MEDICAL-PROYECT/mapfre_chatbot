ecc23c7dfae0d3bfe7e4e48efd5843d989d22ef0
Bernardo Palenciano <b_palenciano_vizcaino@hotmail.com>
2023-09-07 19:27:20 +0200

    secrets case sensitive

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ import streamlit as st
 # from dotenv import load_dotenv
 # load_dotenv()
 
-PINECONE_API_KEY: Final[str] = st.secrets["pinecone_api_key"]
-PINECONE_API_ENV: Final[str] = st.secrets["pinecone_api_env"]
-OPENAI_API_KEY: Final[str] = st.secrets["openai_api_key"]
-DETA_KEY: Final[str] = st.secrets["deta_key"]
+PINECONE_API_KEY: Final[str] = st.secrets["PINECONE_API_KEY"]
+PINECONE_API_ENV: Final[str] = st.secrets["PINECONE_API_ENV"]
+OPENAI_API_KEY: Final[str] = st.secrets["OPENAI_API_KEY"]
+DETA_KEY: Final[str] = st.secrets["DETA_KEY"]
 INDEX_NAME: Final[str] = 'thevalleyopenai'
