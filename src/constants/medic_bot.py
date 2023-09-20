3119e6d985a322f13d6ec3bfbd6857d30fdc4697
Bernardo Palenciano <b_palenciano_vizcaino@hotmail.com>
2023-09-20 20:52:06 +0200

    hybrid search

--- src/constants/medic_bot.py
+++ src/constants/medic_bot.py
@@ -7,6 +7,6 @@ class MedicBotConstants:
     PINECONE_API_ENV: Final[str] = st.secrets["PINECONE_API_ENV"]
     OPENAI_API_KEY: Final[str] = st.secrets["OPENAI_API_KEY"]
     DETA_KEY: Final[str] = st.secrets["DETA_KEY"]
-    INDEX_NAME: Final[str] = 'thevalleyopenai'
+    INDEX_NAME: Final[str] = 'thevalleyembeddings'
     ITERATIONS = 4
 
