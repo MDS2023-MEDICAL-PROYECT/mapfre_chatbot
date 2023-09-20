80111c7543bfdb374bb164ce82cfeef272180ce2
Bernardo Palenciano <131517062+BernardoPalenciano@users.noreply.github.com>
2023-09-20 19:21:13 +0200

    Connecting ana vector database (#8)
    connect to ana vector database and hybrid search

--- streamlit_app_chain.py
+++ streamlit_app_chain.py
@@ -5,7 +5,9 @@ import streamlit as st
 import datetime
 
 from langchain import PromptTemplate, LLMChain, OpenAI
-
+from langchain.retrievers import PineconeHybridSearchRetriever
+from pinecone_text.sparse import BM25Encoder
+from langchain.vectorstores.base import VectorStoreRetriever
 import database as db
 
 from pathlib import Path
@@ -60,18 +62,18 @@ def init_vectorstore():
 
 def get_vectordb():
     index = init_vectorstore()
-    vectordb = Pinecone(
-        index=index,
-        embedding_function=OpenAIEmbeddings(
-            openai_api_key=OPENAI_API_KEY).embed_query,
-        text_key="text"
-    )
+    vectordb = PineconeHybridSearchRetriever(
+            embeddings=OpenAIEmbeddings(
+                        model="text-embedding-ada-002",
+                        openai_api_key=OPENAI_API_KEY)
+            , sparse_encoder=BM25Encoder().default(), index=index, top_k=10, alpha=0
+            )
     return vectordb
 
 
 def get_conversation_chain(vectordb):
     llm = ChatOpenAI(temperature=0)
-    retriever = vectordb.as_retriever()
+    retriever = vectordb
 
     if "memory" not in st.session_state:
         st.session_state.memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
@@ -117,7 +119,7 @@ def get_conversation_chain(vectordb):
 
 def get_diagnosis(vectordb, symptoms):
     llm_diagnosis = OpenAI(temperature=0)
-    retriever = vectordb.as_retriever()
+    retriever = vectordb
 
     diagnosis_template = """Given the following symptoms of a patient known as human and the knowledge given in the Relevant 
     Medical texts, give a 3 possible diagnosis and a number from 0 to 100 with the confidence level you have in the diagnosis.
